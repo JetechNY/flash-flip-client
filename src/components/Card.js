dd8d266b87349871e69b03060a5f421fe892fb0c
JetechNY <sunjetliu@gmail.com>
2020-11-29 15:59:37 -0500

    card flip

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,19 @@ import React from 'react'
 
 class Card extends React.Component{
 
+
+    state = {
+        flipped: true
+    }
+
+    clickFlip = () => {
+        this.setState({flipped: !this.state.flipped})
+    }
+
     render(){
         return(
-            <div className="card">
-                <h2>{this.props.card.term}</h2>
+            <div onClick={this.clickFlip} className="card">
+                <h3>{ this.state.flipped ? this.props.card.term : this.props.card.definition }</h3>
             </div>
         )
     }
