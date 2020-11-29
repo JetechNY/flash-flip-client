86d607becfc08d711138ffa565c8ce069b83e727
billyott <willrichott@gmail.com>
2020-11-29 15:32:25 -0500

    clean up index.html

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -18,13 +18,22 @@ class GameContainer extends React.Component {
                     <div className="timer">
                         timer: 00:00sec
                     </div>
-                </section>
-                <section className="game-container-middle-section">
-                    <div className="card">
-                        <Card key={this.state.currentCard.id} card={this.state.currentCard} />
+                    <div className="game-stats">
+                        <p>right: 0</p>
+                        <p>wrong: 0</p>
+                        <p>skipped: 0</p>
                     </div>
+                    <button className="star-card-button">Star Card</button>
                 </section>
                 <section className="game-container-right-section">
+                    <div className="card">
+                        <Card key={this.state.currentCard.id} card={this.state.currentCard} />
+                        <div className="game-buttons">
+                            <button className="right-button">RIGHT</button>
+                            <button className="wrong-button">WRONG</button>
+                            <button className="skip-button">SKIP</button>
+                        </div>
+                    </div>
                 </section>
             </section>
         )
