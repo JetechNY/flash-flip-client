518d54e5553e7fae105f9dce96cf6b4a017dc1c5
billyott <willrichott@gmail.com>
2020-11-29 19:54:05 -0500

    built initial logic for game

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -4,16 +4,17 @@ import Card from '../components/Card'
 class GameContainer extends React.Component {
 
     state = {
-        currentCard: '',
+        currentCardIndex: 0,
         shuffledCards: [],
         rightCounter: 0,
         wrongCounter: 0,
-        skippedCounter: 0
+        skippedCounter: 0,
+        completedCards: []
     }
 
     componentDidMount() {
         let shuffledCards = this.shuffleCards(this.props.cards)
-        this.setState({currentCard: shuffledCards[0], shuffledCards: shuffledCards})
+        this.setState({shuffledCards: shuffledCards})
     }
 
     shuffleCards = (cards) => {
@@ -31,7 +32,10 @@ class GameContainer extends React.Component {
         return cards;
     }
 
-
+    findIndex = (currentCard) => {
+        let index = this.state.shuffledCards.findIndex(card => card.id === currentCard.id)
+        return index
+    }
 
     handleStarCard = () => {
         
@@ -40,7 +44,7 @@ class GameContainer extends React.Component {
             false: true
         }
 
-        fetch(`http://localhost:3000/cards/${this.state.currentCard.id}`, {
+        fetch(`http://localhost:3000/cards/${this.state.shuffledCards[this.state.currentCardIndex].id}`, {
                 method: 'PATCH',
                 headers: {
                     'Content-Type': 'application/json',
@@ -49,12 +53,58 @@ class GameContainer extends React.Component {
                 body: JSON.stringify({...this.state.currentCard, is_starred: starMatrix[this.state.currentCard.is_starred]})
             })
             .then(resp => resp.json())
-            .then(card => this.setState({currentCard: card}))
+            .then(card => {
+                let index = this.findIndex(card)
+                if (index > -1) {
+                    const newShuffledCards = this.state.shuffledCards
+                    newShuffledCards[index] = card
+                    this.setState({shuffleCards: newShuffledCards})
+                }
+            })
             .catch(err => console.log(err))
 
     } 
 
+    handleRight = () => {
+        const currentCard = this.state.shuffledCards[this.state.currentCardIndex]
+        this.setState(prevState => {
+            return ({
+                rightCounter: prevState.rightCounter += 1,
+                currentCardIndex: prevState.currentCardIndex += 1,
+                completedCards: [...prevState.completedCards, {...currentCard, right: true}]
+            })
+        })
+    }
+
+    handleWrong = () => {
+        const currentCard = this.state.shuffledCards[this.state.currentCardIndex]
+        this.setState(prevState => {
+            return ({
+                wrongCounter: prevState.wrongCounter += 1,
+                currentCardIndex: prevState.currentCardIndex += 1,
+                completedCards: [...prevState.completedCards, {...currentCard, right: false}]
+            })
+        })
+    }
+
+    handleSkipped = () => {
+        const currentCard = this.state.shuffledCards[this.state.currentCardIndex]
+        this.setState(prevState => {
+            return ({
+                skippedCounter: prevState.skippedCounter += 1,
+                currentCardIndex: prevState.currentCardIndex += 1,
+                shuffledCards: [...prevState.shuffledCards, currentCard]
+            })
+        })
+    }
+
+    reviewGame = () => {
+        return this.state.completedCards.map(card => <Card key={card.id} card={card} wonStatus={card.right}/>)
+    }
+
     render() {
+        const currentCard = this.state.shuffledCards[this.state.currentCardIndex]
+        if (!currentCard && !this.state.completedCards.length) return null
         return (
             <section className="game-container">
                 <section className="game-container-left-section">
@@ -66,17 +116,25 @@ class GameContainer extends React.Component {
                         <p>wrong: {this.state.wrongCounter}</p>
                         <p>skipped: {this.state.skippedCounter}</p>
                     </div>
-                    <button className="star-card-button" onClick={this.handleStarCard}>{this.state.currentCard.is_starred ? "Unstar Card": "Star Card"}</button>
+                    {currentCard ? <button className="star-card-button" onClick={this.handleStarCard}>{currentCard.is_starred ? "Unstar Card": "Star Card"}</button> : null}
                 </section>
                 <section className="game-container-right-section">
-                    <div className="game-container-card-holder">
-                        <Card key={this.state.currentCard.id} card={this.state.currentCard} />
-                    </div>
-                    <div className="game-buttons">
-                        <button className="right-button">RIGHT</button>
-                        <button className="wrong-button">WRONG</button>
-                        <button className="skip-button">SKIP</button>
-                    </div>
+                    {this.state.currentCardIndex+1 > this.state.shuffledCards.length ? 
+                        <div className="game-container-card-holder-over">
+                            {this.reviewGame()}
+                        </div>
+                        :
+                        <>
+                            <div className="game-container-card-holder">
+                                <Card key={currentCard.id} card={currentCard} />
+                            </div>
+                            <div className="game-buttons">
+                                <button className="right-button" onClick={this.handleRight}>RIGHT</button>
+                                <button className="wrong-button" onClick={this.handleWrong}>WRONG</button>
+                                <button className="skip-button" onClick={this.handleSkipped}>SKIP</button>
+                            </div>
+                        </>
+                    }
                 </section>
             </section>
         )
