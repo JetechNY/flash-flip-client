4a6582a87fea38450e8149c10afdbedf757abba1
JetechNY <sunjetliu@gmail.com>
2020-12-03 14:32:13 -0500

    profile

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -64,14 +64,14 @@ class LoginForm extends React.Component {
                 <form className="login-form" onSubmit={this.localHandleLoginFormSubmit}>
                     <input className="login-input" name="usernameSignIn" value={this.state.usernameSignIn} onChange={this.handleFormChange} placeholder="username"></input>
                     <input className="login-input" name="passwordSignIn" value={this.state.passwordSignIn} onChange={this.handleFormChange} placeholder="password" type="password"></input>
-                    <button className="login-button">log in</button>
+                    <button className="login-button">Log In</button>
                 </form>
                 <p className="login-form-separator">...or sign up!</p>
                 <form className="create-account-form" onSubmit={this.localHandleSignUpFormSubmit}>
                     <input className="create-account-input" name="emailSignUp" value={this.state.emailSignUp} onChange={this.handleFormChange} placeholder="email address"></input>
                     <input className="create-account-input" name="usernameSignUp" value={this.state.usernameSignUp} onChange={this.handleFormChange} placeholder="username"></input>
                     <input className="create-account-input" name="passwordSignUp" value={this.state.passwordSignUp} onChange={this.handleFormChange} placeholder="password" type="password"></input>
-                    <button className="create-account-button">sign up</button>
+                    <button className="create-account-button">Sign Up</button>
                 </form>
             </section>
         )
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,72 @@
+import { render } from '@testing-library/react'
 import React from 'react'
 
-function Profile(props) {
+class Profile extends React.Component {
+
+    state = {
+        username: "",
+        email: "",
+        password: ""
+    }
+
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        fetch(`http://localhost:3000/users/${this.props.user.id}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                Authorization: `Bearer ${this.props.jwt}`
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+                email: this.state.email,
+                password: this.state.password
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => {console.log("return", data)})
+        .catch(err => console.log(err))
+        e.target.reset()
+    }
 
 
-    return(
-        <h1>Hello, {props.user.username}.</h1>
-    )
+    render(){
+        console.log("orig props", this.props.user)
+        return(
+            <section className="profile-container">
+                <h1>Hello, {this.props.user.username}.</h1>
+                <h4>Please update your information below</h4>
+                <div className="edit-container">
+                    <form onSubmit={this.submitHandler} className="edit-form">
+                        <div>
+                        Enter a new Username:
+                        <input type="text" name="username" placeholder="Enter a new Username" className="input-text" value={this.state.name} onChange={this.changeHandler}/>
+                        </div>
+                        <div>
+                        Enter a new Email:
+                        <input type="text" name="email" placeholder="Enter a new Email" className="input-text" value={this.state.email} onChange={this.changeHandler}/>
+                        </div>
+                        <div>
+                        Enter a new Password:
+                        <input type="password" name="password" placeholder="Enter a new Password" className="input-text" value={this.state.password} onChange={this.changeHandler}/>
+                        </div>
+                        <div>
+                        <input type="submit" name="submit" value="Update User Info" className="submit"/>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        )
+    }
 }
+
+
 
 export default Profile;
